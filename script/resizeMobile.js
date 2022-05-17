74bf5068697874ed5e7ff07590e9735bfd4f662b
JuanDaMB <david.juan.david.1@gmail.com>
2022-05-17 16:00:04 -0500

    Added some views

---
+++ script/resizeMobile.js
@@ -0,0 +1,7 @@
+window.onresize = change;
+function change(event) {
+    if(window.innerWidth > 1200)
+    {
+        location.href = "menuprincipal_esc.html";
+    }
+}
---
+++ script/script.js
@@ -0,0 +1,4 @@
+function hide(){
+  var element = document.getElementById("gameView");
+  element.classList.toggle("hide");
+}
